ebaf0ac8f34c2a751ff1518f3340ab8ceb3faab4
Zahid Arif <zahidarif976@gmail.com>
2026-09-03 05:33:58 -0800

    Add files via upload

---
+++ lec4.py
@@ -0,0 +1,14 @@
+#dictonary
+# info = {
+#     "name":"zahid",
+#     "class":"bsse",
+#     "gpa"  : 3.1
+   
+# }
+# print(info)
+
+#sets
+
+set1 = {1,2,3,}
+set2 = {4,5,6,}
+print(set1.union(set2))
